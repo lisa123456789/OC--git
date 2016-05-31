2377f55b7be5775d67f3e405d5f99f80d361aef1
lisa123456789 <596289441@qq.com>
2016-05-31 09:39:51 -0700

    init

---
+++ OC-git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OC-git
+//
+//  Created by Mac on 16/5/31.
+//  Copyright © 2016年 琼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OC-git/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  OC-git
+//
+//  Created by Mac on 16/5/31.
+//  Copyright © 2016年 琼. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    //这是初始化创建
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
